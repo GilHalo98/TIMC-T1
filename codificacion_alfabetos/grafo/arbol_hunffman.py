8548065c4e616434774448a4601102bc4c0c618f
Diego Gil <diego_gilhalo98@homail.com>
2021-10-10 19:00:49 -0500

    Generacion del codigo de los caracteres agregada

--- codificacion_alfabetos/grafo/arbol_hunffman.py
+++ codificacion_alfabetos/grafo/arbol_hunffman.py
@@ -33,6 +33,22 @@ class Arbol_Hunffman(object):
         # Diccionario de caracteres.
         self.diccionario = {}
 
+        # Diccionario de codigos para encriptar.
+        self.diccionario_codigos = {}
+
+    # Retorna la codificacion hasta un caracter, esto se hace con el costo
+    # de las aristas, dada una ruta a seguir.
+    def __get_codificacion(self, ruta):
+        codigo = ''
+
+        for i in range(len(ruta) - 1):
+            if self.topologia[ruta[i]].hijo_izquierda == ruta[i + 1]:
+                codigo += '0'
+            else:
+                codigo += '1'
+
+        return codigo
+
     # Crea el arbol binario desde un diccionario.
     def generar_arbol(self, frecuencias):
         # Agregamos las hojas del arbol.
@@ -68,12 +84,19 @@ class Arbol_Hunffman(object):
                 + frecuencias[frontera[1]]
             )
 
+            if frontera[1] <= frontera[0]:
+                hi = frontera[1]
+                hd = frontera[0]
+            else:
+                hi = frontera[0]
+                hd = frontera[1]
+
             # Se genera un nuevo nodo conteniendo el nuevo omega.
             self.topologia[nuevo_id] = Nodo(
                 nuevo_id,
                 frecuencias[nuevo_id],
-                hijo_izquierda=frontera[1],
-                hijo_derecha=frontera[0]
+                hijo_izquierda=hi,
+                hijo_derecha=hd
             )
 
             # Aumenta la cantidad de nodos en 1.
@@ -95,12 +118,21 @@ class Arbol_Hunffman(object):
         # Por ultimo, la raiz es el ultimo nodo generado.
         self.id_raiz = nuevo_id
 
+        # Lo ultimo que queda es generar los codigos del encriptado.
+        for id_caracter, codigo in self.codigos():
+            self.diccionario_codigos[self.diccionario[id_caracter]] = codigo
+
     # Grafica el arbol de hunffman.
     def graficar(self, archivo_salida):
+        codificacion = {}
+        for id, codigo in self.codigos():
+            codificacion[id] = codigo
+
         grafico = Graph(
             comment='Test',
             format='svg',
         )
+        grafico.node_attr['shape'] = 'circle'
 
         # Lista todos los id de nodos en el arbol.
         nodos = list(self.topologia.values())
@@ -115,7 +147,7 @@ class Arbol_Hunffman(object):
                 # Se agrega el nodo en el grafico.
                 grafico.node(
                     str(nodo.id_nodo),
-                    '{}\n{}'.format(nodo.id_nodo, float(nodo.contenido))
+                    str('{0:.2f}'.format(float(nodo.contenido)))
                 )
 
                 # Hijo de la izquierda.
@@ -133,15 +165,74 @@ class Arbol_Hunffman(object):
                 )
 
             else:
+                contenido = '{{'
+                contenido += self.diccionario[nodo.id_nodo]
+                contenido += ' | '
+                contenido += str('{0:.2f}'.format(float(nodo.contenido)))
+                contenido += ' } | '
+                contenido += codificacion[nodo.id_nodo] + ' }'
+
                 # Si el nodo es una hoja, se agrega tambien su caracter.
                 grafico.node(
                     str(nodo.id_nodo),
-                    '{}\n{}\n{}'.format(
-                        nodo.id_nodo,
-                        self.diccionario[nodo.id_nodo],
-                        float(nodo.contenido),
-                    )
+                    label=contenido,
+                    shape='Mrecord'
                 )
 
         # Por ultimo renderiza el grafo en un archivo svg.
         grafico.render(filename=archivo_salida)
+
+    # Utiliza DFS para retornar la codificacion de los caracteres.
+    def codigos(self):
+        # Almacena la ruta en donde se encuentra, con esto podemos
+        # saber la codificacion del caracter.
+        ruta = []
+
+        # Lista de nodos visitados.
+        visitados = []
+
+        # Cantida de nodos visitados, controla el ciclo.
+        total_visitados = 0
+
+        # Nodo donde se comienza el DFS
+        id_nodo = self.id_raiz
+
+        # Mientas que queden nodos por visitar.
+        while total_visitados < self.total_nodos:
+
+            # Se agrega el nodo a la ruta.
+            ruta.append(id_nodo)
+
+            # Si el nodo no esta marcado como visitado, se agrega.
+            if id_nodo not in visitados:
+                visitados.append(id_nodo)
+                total_visitados += 1
+
+            # En este punto, si el nodo es una hoja, retorna la codificacion
+            # del caracter, para esto se usa la ruta y el peso de las aristas.
+            if self.topologia[id_nodo].es_hoja():
+                # Se usan generadores para que no genere tanta memoria.
+                yield id_nodo, self.__get_codificacion(ruta)
+
+                # Regresamos una posicion en la ruta.
+                id_nodo = ruta.pop()
+                id_nodo = ruta.pop()
+
+            # Si no es una hoja.
+            else:
+                if self.topologia[id_nodo].hijo_izquierda not in visitados:
+                    id_nodo = self.topologia[id_nodo].hijo_izquierda
+                elif self.topologia[id_nodo].hijo_derecha not in visitados:
+                    id_nodo = self.topologia[id_nodo].hijo_derecha
+                else:
+                    # Regresamos una posicion en la ruta.
+                    id_nodo = ruta.pop()
+                    id_nodo = ruta.pop()
+
+    # Encripta un texto dado con el arbol de Huffman.
+    def encriptar(self, texto):
+        texto_encriptado = ''
+        for c in texto:
+            texto_encriptado += self.diccionario_codigos[c]
+
+        return texto_encriptado
