4f8e6290da2a7e35d0e5442484eb295021c496c2
Diego Gil <diego_gilhalo98@homail.com>
2022-09-15 12:39:14 -0500

    Main modificado

--- infoYentro/main.py
+++ infoYentro/main.py
@@ -1,11 +1,10 @@
 '''
-    Entropia e información mutua.
+    Generador de instancias.
 
     Parametros:
-        - Intancia a procesar.
-        - Tipo de operacion, usado unicamente al cargar csv.
-        - Si no se ingresa ninguno de estos dos parametros, entrara
-            en modo de captura de datos manual.
+        - Tipo de proceso.
+        - Dimencion de array / matriz.
+        - Nombre de archivo de salida, opcional.
 '''
 
 
@@ -14,289 +13,188 @@
 
 
 # Librerias estandar.
-import sys
-import pickle
+import os
 import time
-from os import system
+import pickle
+import pathlib
+import argparse
 
 # Librerias de tercer
 import numpy as np
+import pandas as pd
 
 # Librerias propias.
-from util.funciones import entropia
+from util.tui import data_input
+from util.funciones import Ie, He
 from util.constantes import Tipo_Operacion
+from util.archivos import cargar_instancia, cargar_instancia_csv
 
 
-# Toma por parte del usuario el tipo de operación.
-def get_tipo_operacion():
-    seleccion = None
+# Formatea los argumentos pasados por consola.
+def format_args() -> argparse.Namespace:
+    args_parser = argparse.ArgumentParser(
+        description='Programa principal de Información mutua y entropia'
+    )
 
-    while not seleccion:
-        system('clear')
-        print('Seleccionar un tipo de proceso:')
-        print('\t1.- Cuantificable.')
-        print('\t2.- Transmición de datos.')
-        print('\t3.- Transicion de estados.')
-        seleccion = input('Seleccionar el numero |---> ')
+    args_parser.add_argument(
+        '--instancia',
+        default=None,
+        help='Nombre de la instancia',
+        dest='instancia',
+        type=str,
+    )
 
-        if seleccion == '1':
-            operacion = Tipo_Operacion.CUANTIFICABLE
-        elif seleccion == '2':
-            operacion = Tipo_Operacion.TRANSMISION_DATOS
-        elif seleccion == '3':
-            operacion = Tipo_Operacion.TRANSICION_ESTADOS
-        else:
-            seleccion = None
+    args_parser.add_argument(
+        '--tipo_operacion',
+        default=None,
+        help='''
+            Tipo de operacion de la instancia
+                1: cuantificable
+                2: transmicion de datos
+                3: tansicion de estados
+        ''',
+        dest='tipo_operacion',
+        type=int,
+    )
 
-    return operacion
+    args = args_parser.parse_args()
 
-
-def verificar_seleccion(seleccion, datos):
-    alerta = ''
-    # Verifica que se realizara una seleccion.
-    if seleccion != '':
-        if seleccion[0] == 'f':
-            # Se indica que el programa saldra del ingreso de datos.
-            pass
-
-        elif seleccion[0] == 'd':
-            # Elimina un dato en un index dado.
-            index = seleccion.split('d')[-1]
-
-            if '.' in index:
-                # El index debe ser un entero.
-                alerta = 'El index debe ser un entero, no un flotante '
-            else:
-                index = int(index)
-                if index < 0 or index >= len(datos):
-                    #  El index debe ser menor que la cantida de
-                    # datos total y mayor que 0.
-                    alerta = 'El index debe ser mayor que 0 y menor que {}'
-                    alerta = alerta.format(len(datos))
-                else:
-                    dato_eliminado = datos.pop(index)
-                    alerta = 'El elemento {} con index {} fue eliminado '
-                    alerta = alerta.format(dato_eliminado, index)
-
-            seleccion = None
-
-        elif seleccion[0] == 'a':
-            # Agrega un dato en un index dado.
-            dato = float(seleccion.split('d')[-1])
-            index = seleccion.split('d')[0][1:]
-
-            if '.' in index:
-                # El index debe ser un entero.
-                alerta = 'El index debe ser un entero, no un flotante '
-            else:
-                index = int(index)
-                if index < 0 or index >= len(datos):
-                    #  El index debe ser menor que la cantida de
-                    # datos total y mayor que 0.
-                    alerta = 'El index debe ser mayor que 0 y menor que {}'
-                    alerta = alerta.format(len(datos))
-                else:
-                    # Si el dato es valido para procesar,
-                    # se agrega a los datos.
-                    if dato != 0 and dato != -1:
-                        datos.insert(index, dato)
-                        alerta = 'El elemento {} con index {} fue agregado'
-                        alerta = alerta.format(dato, index)
-                    else:
-                        alerta = 'Solo agregar datos diferentes de -1 y 0'
-
-            seleccion = None
-
-        elif seleccion[0] == 'm':
-            # Modifica un dato en un index dado.
-            dato = float(seleccion.split('d')[-1])
-            index = seleccion.split('d')[0][1:]
-
-            if '.' in index:
-                # El index debe ser un entero.
-                alerta = 'El index debe ser un entero, no un flotante '
-            else:
-                index = int(index)
-                if index < 0 or index >= len(datos):
-                    #  El index debe ser menor que la cantida de
-                    # datos total y mayor que 0.
-                    alerta = 'El index debe ser mayor que 0 y menor que {}'
-                    alerta = alerta.format(len(datos))
-                else:
-                    # Si el dato es valido para procesar,
-                    # se agrega a los datos.
-                    if dato != 0 and dato != -1:
-                        dato_anterior = datos[index]
-                        datos[index] = dato
-                        alerta = 'El elemento {} con index {}'
-                        alerta += ' fue modificado a {}'
-                        alerta = alerta.format(dato_anterior, index, dato)
-                    else:
-                        alerta = 'Solo agregar datos diferentes de -1 y 0'
-
-            seleccion = None
-
-        else:
-            # Se ingresa un dato como tal y se convierte en float.
-            dato = float(seleccion)
-
-            # Si el dato es valido para procesar, se agrega a los datos.
-            if dato != 0 and dato != -1:
-                datos.append(dato)
-            else:
-                alerta = 'Solo agregar datos diferentes de -1 y 0'
-
-            seleccion = None
-    else:
-        alerta = 'Reaiza una seleccion'
-
-    return alerta, seleccion
-
-
-# Ingreso de datos directo del usuario.
-def data_input():
-    # Obtiene el tipo de operacion que ingresa el usuario.
-    operacion = get_tipo_operacion()
-
-    alerta = ''
-    datos = []
-    seleccion = None
-    while not seleccion:
-        system('clear')
-
-        mensaje = '''
-            Ingresa un numero para agregarlo
-            {}
-            {}
-            {}
-            f.- Finaliza el ingreso de datos
-            d + index.- Elimina el dato en el index dado Ej: d0
-            a + index + d + dato.- Agrega el dato en el index dado Ej: a0d10
-            m + index + d + dato.- Modifica el dato en el index dado Ej: m0d10
-        '''.format('-'*len(str(datos)), datos, '-'*len(str(datos)))
-
-        print(mensaje)
-
-        seleccion = input('\n{}|---> '.format(alerta))
-        seleccion = seleccion.lower()
-        alerta = ''
-
-        alerta, seleccion = verificar_seleccion(seleccion, datos)
-
-    return np.array(datos, dtype=float), operacion
+    return args
 
 
 # Funcion main.
-def main(instancia=None, operacion=''):
-    system('clear')
-    if instancia:
-        extencion = instancia.split('.')[1]
-        a = time.perf_counter()
+def main(con_args: argparse.Namespace, *args, **kargs) -> None:
+    # Cargamos los parametros pasados por consola.
+    instancia: str = con_args.instancia
+    tipo_operacion: int = con_args.tipo_operacion
 
-        if extencion == 'csv':
-            print('---> Cargando Instancia csv')
+    # Instanciamos las variables a usar en el calculo de
+    # la entropia y la informacion mutua.
+    frecuencias = None
 
-            mensaje = '\n---> ¡TODOS LOS DATOS QUE SEAN '
-            mensaje += 'nan, 0 y -1 SERAN DESCARTADOS!\n'
-            print(mensaje)
-
-            # Si se Carga desde un csv.
-            datos = np.genfromtxt(instancia, delimiter=',')
-
-            # Removemos los nan.
-            datos = datos[np.logical_not(np.isnan(datos))]
-
-            # Removemos los 0
-            datos = datos[datos != 0]
-
-            # Removemos los -1
-            datos = datos[datos != -1]
-
-            # Se verifica el tipo de operacion.
-            if operacion == 'cuantificable':
-                tipo_operacion = Tipo_Operacion.CUANTIFICABLE
-            elif operacion == 'datos':
-                tipo_operacion = Tipo_Operacion.TRANSMISION_DATOS
-            elif operacion == 'estados':
-                tipo_operacion = Tipo_Operacion.TRANSICION_ESTADOS
-            else:
-                raise Exception(
-                    'El tipo de operación {} no es valido'.format(
-                        operacion
-                    )
-                )
-
-        elif extencion == 'fime':
-            print('---> Cargando Instancia fime')
-            # Se abre el fichero si se carga desde un fim.
-            fichero = open(instancia, "rb")
-
-            # Se carga el archivo serializado.
-            informacion = pickle.load(fichero)
-
-            datos = informacion['datos']
-            tipo_operacion = informacion['tipo operacion']
-
-        else:
-            raise Exception('Extención de archivo {} no soportada'.format(
-                    extencion
-                )
-            )
-
-        b = time.perf_counter()
-
-        print('---> Instancia cargada en {0:.2f}s'.format(b - a))
+    #  Verificamos como se ingresaran los datos.
+    if instancia is None:
+        # Si el directorio del archivo instancia no fue pasado por
+        # consola, entonces los datos seran ingresados manualmente.
+        frecuencias, tipo_operacion = data_input()
 
     else:
-        datos, tipo_operacion = data_input()
-        system('clear')
+        # Instanciamos el directorio de la instancia.
+        directorio = pathlib.Path(instancia)
 
-    print('\n{}\n'.format(datos))
+        if directorio.suffix == '.csv':
+            # Si es un arhcivo .csv, entonces se cargan los datos desde
+            # el arhivo .csv.
 
-    cantidad_datos = 1
-    for i in datos.shape:
-        cantidad_datos *= i
+            # Instanciamos el tipo de operacion que sera la instancia.
+            if tipo_operacion is None:
+                # Tiene que existir un tipo de operacion pasado por
+                # parametros.
+                raise Exception(
+                    '---> Es necesario seleccionar un tipo de operacion!'
+                    + ' ingresa -h para mas información.'
+                )
+
+            elif tipo_operacion == 1:
+                tipo_operacion = Tipo_Operacion.CUANTIFICABLE
+
+            elif tipo_operacion == 2:
+                tipo_operacion = Tipo_Operacion.TRANSMISION_DATOS
+
+            elif tipo_operacion == 3:
+                tipo_operacion = Tipo_Operacion.TRANSICION_ESTADOS
+
+            else:
+                raise Exception('Tipo de operacion invalido!')
+
+            frecuencias = cargar_instancia_csv(directorio)
+
+        elif directorio.suffix == '.fime':
+            # Si es un arhcivo .fime, entonces se cargan los datos desde
+            # el arhivo .fime.
+            datos = cargar_instancia(directorio)
+
+            frecuencias = datos['instancia']
+            tipo_operacion = datos['tipo_operacion']
+
+        else:
+            raise Exception(
+                '---> Tipo de archivo no soportado'
+                + ', unicamente se soportan .csv y .fime'
+            )
 
     unidades = ''
-    tipo = ''
     if tipo_operacion is Tipo_Operacion.CUANTIFICABLE:
-        unidades = 'Hartleys / Símbolo'
-        tipo = 'Cuantificable'
+        unidades = 'hartleys / simbolo'
+
     elif tipo_operacion is Tipo_Operacion.TRANSMISION_DATOS:
-        unidades = 'Bits / Símbolo'
-        tipo = 'Transmisión de datos'
+        unidades = 'bits / simbolo'
+
     elif tipo_operacion is Tipo_Operacion.TRANSICION_ESTADOS:
-        unidades = 'Nats / Símbolo'
-        tipo = 'Transicion de estados'
+        unidades = 'nats / simbolo'
 
-    print('---> Datos cargados: {}'.format(cantidad_datos))
-    print('---> Tipo de problema: {}\n'.format(tipo))
+    print('---> Instancia cargada')
+    print(frecuencias)
+    print('---> Tipo de operacion: {}'.format(tipo_operacion))
 
-    a = time.perf_counter()
+    es_matriz = True if len(frecuencias.shape) > 1 else False
 
-    sumatoria_he, he, sumatoria_ie, ie = entropia(
-        datos,
-        tipo_operacion,
-        len(datos.shape) > 1
+    if es_matriz:
+        print('---> Instancia es matriz, aplanando matriz')
+
+        # Aplanamos la matriz.
+        aux_frecuencias = frecuencias.values.flatten()
+
+        # Aquellos valores con frecuencias 0 se omiten.
+        aux_frecuencias = aux_frecuencias[aux_frecuencias != 0]
+
+        # Asignamos nuevos ids
+        valores = {}
+        i = 0
+        for fi in aux_frecuencias:
+            valores['f{}'.format(i)] = fi
+            i += 1
+
+        # Guardamos los datos en una Serie.
+        frecuencias = pd.Series(valores)
+        print('---> Matriz aplanada')
+        print(frecuencias)
+
+    informacion_mutua = Ie(frecuencias, tipo_operacion)
+    print('---> Información mutua calculada {}'.format(unidades))
+
+    entropia = He(frecuencias, informacion_mutua)
+    print('---> Entropia calculada {}'.format(unidades))
+
+    print('---> Informe creado')
+    informe = pd.DataFrame(
+        {
+            'Pe': frecuencias.values,
+            'Ie': informacion_mutua.values,
+            'He': entropia.values
+        },
+        index=informacion_mutua.index
     )
 
-    b = time.perf_counter()
+    sum_Ie = sum(informacion_mutua)
+    sum_He = sum(entropia)
 
-    print('---> Tiempo en realizar las operaciones: {0:.4f}s'.format(
-            b - a
-        )
-    )
+    print(informe)
+    
+    msj = '---> Información mutua: {0:.2f} '.format(sum(informacion_mutua))
+    msj += unidades
+    print(msj)
 
-    mensaje = '\n---> Ie: {}'.format(ie)
-    mensaje += '\n---> sum Ie: {0:.2f} '.format(sumatoria_ie)
-    mensaje += unidades
-    print(mensaje)
-
-    mensaje = '\n---> He: {}'.format(he)
-    mensaje += '\n---> sum He: {0:.2f} '.format(sumatoria_he)
-    mensaje += unidades
-    print(mensaje)
-
+    msj = '---> Entropia: {0:.2f} '.format(sum(entropia))
+    msj += unidades
+    print(msj)
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    # Limpiamos la consola.
+    os.system('clear')
+
+    # Formateamos los parametros pasados por consola.
+    con_args = format_args()
+
+    # Ejecutamo la funcion main.
+    main(con_args)
